cf0b4de2a5cc80b5967d6429575eb6aff9201aed
AntoineBlanot <antoineblanot@gmail.com>
2022-01-10 15:29:28 +0100

    check the ngrok

--- server.js
+++ server.js
@@ -9,5 +9,6 @@ const fb = new FBeamer(config.FB);
 const server = express();
 const PORT = process.env.PORT || 3000;
 
-server.get('/',(req,res)=> fb.registerHook(req, res));
+// server.get('/',(req,res) => fb.registerHook(req, res));
+server.get('/',(req,res) => res.send("hello !!"));
 server.listen(PORT,()=> console.log(`The bot server is running on port ${PORT}`));
