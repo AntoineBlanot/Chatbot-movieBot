d313767760caca2534d9bf834bb2c00093a775b0
TheFlizziard <theflizziard@gmail.com>
2022-01-17 15:41:33 +0100

    update_movie_bot

--- server.js
+++ server.js
@@ -9,6 +9,11 @@ const fb = new FBeamer(config.FB);
 const server = express();
 const PORT = process.env.PORT || 3000;
 
-// server.get('/',(req,res) => fb.registerHook(req, res));
-server.get('/',(req,res) => res.send("hello !!"));
-server.listen(PORT,()=> console.log(`The bot server is running on port ${PORT}`));
+server.listen(PORT, ()=> console.log(`The bot server is running on port ${PORT}`));
+
+// server.get('/', (req, res) => fb.registerHook(req, res));
+console.log("test");
+server.post('/', (req, res) => fb.incoming(req, res));
+console.log("test 2");
+
+// server.get('/', (req,res) => res.send("hello !!"));
